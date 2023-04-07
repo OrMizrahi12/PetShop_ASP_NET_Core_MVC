e78968c36ff5d6880585610e0e209bd5346861b0
ormizrahi1610 <ormizrahi1610@gmail.com>
2023-04-07 12:41:02 +0300

    after client side validation ro login & register

---
+++ PetShopClient/wwwroot/ValidationForms/LoginFormValidation.js
@@ -0,0 +1,36 @@
+﻿
+document.addEventListener('DOMContentLoaded', function () {
+
+    $.validator.setDefaults({
+        errorClass: 'text-danger',
+    });
+
+    var validator = $("form").validate({
+        rules: {
+            username: {
+                required: true,
+            },
+            password: {
+                required: true,
+            },
+        },
+        messages: {
+            username: {
+                required: "Please enter a username",
+            },
+            password: {
+                required: "Please enter a password",
+            },
+        },
+    });
+    $("#submitBtn").on("click", function (e) {
+        e.preventDefault();
+
+        if (validator.form()) { // Check if form is valid
+            $("form").trigger("submit");
+        } 
+    });
+})
+
+
+
---
+++ PetShopClient/wwwroot/ValidationForms/RegisterFormValidation.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    $.validator.setDefaults({
+        errorClass: 'text-danger',
+    });
+
+    // Initialize form validation
+    var validator = $("form").validate({
+        rules: {
+            username: {
+                required: true,
+            },
+            password: {
+                required: true,
+                minlength: 6, // Password must be at least 6 characters long
+            },
+            confirmPassword: {
+                required: true,
+                equalTo: "#password", // Confirm password must match password
+            },
+        },
+        messages: {
+            username: {
+                required: "Please enter a username",
+            },
+            password: {
+                required: "Please enter a password",
+                minlength: "Password must be at least 6 characters long",
+            },
+            confirmPassword: {
+                required: "Please confirm your password",
+                equalTo: "Passwords do not match",
+            },
+        },
+    });
+
+    // Handle form submission
+    $("#submitBtn").on("click", function (e) {
+        e.preventDefault();
+
+        if (validator.form()) { // Check if form is valid
+            $("form").trigger("submit");
+        }
+    });
+})
